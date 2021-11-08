e595e243edbd7340796926b8772283c202e4799f
diegojojoayandun <3922@holbertonschool.com>
2021-11-08 16:00:15 -0500

    Add main.h header file to repository

---
+++ main.h
@@ -0,0 +1,29 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * match_especifier - structure to match a specifier with respective function
+ * related with the especifier
+ * @esp: especifier (%s,%c,%d, ...)
+ * @fp: pointer to corresponfind function
+ */
+struct match_especifier
+{
+	char esp;
+	int (*fp)();
+};
+
+typedef struct match_especifier match_t;
+
+int _putchar(char c);
+int _strlen(const char *s);
+int print_string(va_list l);
+int print_char(va_list l);
+int print_percent(void);
+int _printf(const char *format, ...);
+
+#endif
