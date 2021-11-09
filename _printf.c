33ee0a35cc3eff8c58b40bd82de77e793c55d877
diegojojoayandun <3922@holbertonschool.com>
2021-11-08 21:19:28 -0500

    Add man _printf

--- _printf.c
+++ _printf.c
@@ -18,7 +18,6 @@ int _printf(const char *format, ...)
 
 	va_start(ap, format);
 
-	;
 	if (!format || (format[0] == '%' && !format[1]))
 		return (-1);
 	if (format[0] == '%' && format[1] == ' ' && !format[2])
