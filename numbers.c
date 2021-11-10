f54a032ffd78035b15e5121dea5a10a5ef01ee18
diegojojoayandun <3922@holbertonschool.com>
2021-11-10 06:50:12 -0500

    Fix error validation 0 value

--- numbers.c
+++ numbers.c
@@ -55,7 +55,12 @@ void print_number(int n)
 	else
 		n1 = n;
 
-	if (n1 / 10)
-		print_number(n1 / 10);
-	_putchar((n1 % 10) + '0');
+	if (n != 0)
+	{
+		if (n1 / 10)
+			print_number(n1 / 10);
+		_putchar((n1 % 10) + '0');
+	}
+	else
+		_putchar(n + '0');
 }
