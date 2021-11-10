47ff3a5545008092ae97371f483fb9de41d24bc6
diegojojoayandun <3922@holbertonschool.com>
2021-11-10 09:21:27 -0500

    Update and fix some error and bugs

--- main.h
+++ main.h
@@ -26,14 +26,14 @@ typedef struct match_especifier match_t;
 
 int _putchar(char c);
 int _strlen(const char *s);
-int print_string(va_list l);
-int print_char(va_list l);
+int print_string(va_list list);
+int print_char(va_list list);
 int print_percent(void);
 int _printf(const char *format, ...);
 int (*get_function(char s))(va_list);
 
 int count_chiper(int n);
 int print_integer(va_list list);
-void print_number(int n);
+void op_number(int n);
 
 #endif
